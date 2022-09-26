014f3eb42bd12738abf2992ee1458d6a99144484
Fernanda Palapa <fernandabqd@gmail.com>
2022-09-26 10:58:51 -0500

    ci:anidar script

---
+++ script.js
@@ -0,0 +1,6 @@
+  console.log("Saoko papi saoko");
+      document.getElementById("generation").style.color="grey";
+      function colorTitulo(){
+        console.log("Pulsó el botón de Generation");
+        document.getElementById("generation").style.color = "red";
+      }
